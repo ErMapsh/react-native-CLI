f90b3d48478bf4a82abcd31e24c9e2d87c1738c0
ErMapsh <maheshmestri73@gmail.com>
2022-01-16 20:31:51 +0530

    hello world

--- App.js
+++ App.js
@@ -3,8 +3,29 @@ import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={style.container}>
+      <Text style={style.Text}>Hello World</Text>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  Text :{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 55,
+    color: 'black',
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    margin: 55,
+    padding: 15,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 14,
+
+  }
+})
